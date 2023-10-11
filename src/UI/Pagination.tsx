4283123e72b4092af10016b8c8eef168b194024b
Сергей <privet15.89@mail.ru>
2023-10-11 19:21:24 +0500

    fix: props issue

--- src/UI/Pagination.tsx
+++ src/UI/Pagination.tsx
@@ -48,12 +48,15 @@ export const Pagination: React.FC<Props> = ({
             <button disabled={currentPage === 1} onClick={prevHandler}>prev</button>
             {pages.map(p =>
                 <Page key={p}
-                      active={p === currentPage ? "true" : ""}
-                      onClick={() => onClickHandler(p)}>{p}</Page>
+                      active={p === currentPage}
+                      onClick={() => onClickHandler(p)}>
+                    {p}
+                </Page>
             )}
             <button disabled={currentPage === pagesCount} onClick={nextHandler}>next</button>
 
-            <select defaultValue={"10"} onChange={(e) => onSelectChangeHandler(e)} name="page_size"
+            <select defaultValue={"10"} onChange={(e) => onSelectChangeHandler(e)}
+                    name="page_size"
                     id="page_size">
                 <option value="5">5</option>
                 <option value="10">10</option>
@@ -71,9 +74,11 @@ const Pages = styled.div`
   gap: 15px;
 `
 
-const Page = styled.button<{ active: string }>`
+const Page = styled.button<{ active?: boolean }>`
   ${props => props.active && css`
-    background: limegreen;
-    border: none`
-  }
+    background-color: aquamarine;
+    border: none;
+    padding: 3px;
+  `}
+  
 `
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
+import {StyleSheetManager} from "styled-components";
+import isPropValid from "@emotion/is-prop-valid"
 import {GlobalStyle} from "./styles/GlobalStyles";
 import {App} from './App.tsx'
 import './index.css'
@@ -10,7 +12,9 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
-        <App/>
+        <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
+            <App/>
+        </StyleSheetManager>
         <ReactQueryDevtools initialIsOpen={false}/>
         <GlobalStyle/>
     </QueryClientProvider>
